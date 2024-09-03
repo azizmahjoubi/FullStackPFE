f30bcc57b947ef4f56e571f89d78a1b49f770580
azizmahjoubi <61149774+azizmahjoubi@users.noreply.github.com>
2024-09-03 14:35:00 +0100

    Update test.server.js

--- express/test/test.server.js
+++ express/test/test.server.js
@@ -3,12 +3,17 @@ const request = require('supertest');
 const { server } = require('../server');
 
 describe('Server Tests', function() {
+  this.timeout(10000); // Setting the timeout for all tests
+
+  after(function(done) {
+    server.close(done); // Close the server after tests are done
+  });
+
   it('should return status 200 for GET /', function(done) {
     request(server)
       .get('/')
       .expect(200, done);
-    this.timeout(10000); 
   });
 
-  // Add more test cases as per your application's logic
+  // Add more test cases
 });
