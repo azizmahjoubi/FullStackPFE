c849b3720a55d1816c7d9c50cb9c6ad9560b84f3
azizmahjoubi <61149774+azizmahjoubi@users.noreply.github.com>
2024-09-03 15:53:57 +0100

    Update server.js

--- express/server.js
+++ express/server.js
@@ -39,7 +39,7 @@ app.get("/", (req, res) => {
 require("./app/routes/serveur.routes")(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3000;
 const server = app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}.`);
 });
